d5eb8e0e2ce782a8ca49a94a5fbe66447b4c1b36
Grant Eckett <grant.eckett@gmail.com>
2024-08-17 09:58:43 +0100

    Complete

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Track Trainer
- Final Project for CS50 - a Flask based web app that supports drivers with learning race tracks.
+# TrackTrainer
+#### Video Demo: (https://www.youtube.com/watch?v=2FxlBYEG5XE)
+#### Description:
+
+This CS50 final project was made to allow me to demonstrate the lessons learnt on this course in the context of one of my main interests-motorsport.
+
+For the project my goal was to implement a web based application using Flask and SQL that would allow the user to save notes on tracks that they are driving in real world or virtually and work towards reducing their lap times.
+
+To achieve the goal of the project I used Python Flask alongside HTML ,CSS and SQLachamy via VS Code.Python is widely used in industry today, so making it the language of choice for this project felt like the right decision as the final result will be used as part of my portfolio for job applications into software development roles.The structure of the app follows the standard Flask convention of having files divided into static,instance and template folders with a app.py file containing the code required to launch the application in browser.
+
+Within the instance folder is the app’s database, which contains tables for registering users, allowing them to add tracks to their account and making corner by corner notes for those tracks.These tables are created by classes in the app.py using the flask sqlachemy module.
+
+The static folder contains the images used for the promotional element of the home page for the web app alongside a style.css file that formats all of the key html elements of the web app.For ease of buildling the front end,Bootstrap styles have been used where possible.
+
+Most of the project comprises of content within the template folder, made up of the multiple HTML files that make up the front end seen by the end user.To ensure the format is consistent across all pages, a layout .html file is used with Jinga notation to set a navigation bar and footer that is applied to all pages.
+
+Jinga is used to alter the navigation bar based on whether a user is logged in or not as part of access restriction.Function decorators are also used within app.py to prevent users manipulating the htmls to gain access to areas without passing standard login validation.
+
+The homepage (index.html) also uses Jinga to alter the content based on login.Registered users will see their list of saved tracks and have access to their notes along with the ability to go to a separate html to edit notes and track times direct from the home page.New users will see a carousel of promotional images and captions to entice them to register and become a user. 
+
+The backbone of the app is its database, which needs to be updated by user interaction.For this there are front end htmls(add,notes and register.html) which give the user input forms to add tracks, notes per corner and specific driving event (braking,turn in,exit),update their best lap times as they improve and register to the app to use these features.
+
+All input forms feature placeholders to clearly direct the user on the format in which the data should be submitted and what content they should be adding to get the most out of their experience.
+
+In the backend, the database updates and addition of new entries are conducted by using the Flask SQLachemy framework. For updates, a query is run to find the existing information, then the new information is added using the suitable class notation (e.g class.variable) before committing to the database.Updates can be made to notes and lap times.
+
+New entries also go through a query check to avoid duplication of data before entering the database via the creation of a new class for the given table This is usually through the creation of a variable like new_track or new_user.
+
+All queries utilise the id of users in connection with a variable such as track_name.In terms of data types, strings are mainly used with the occasional numeric value for variables such as user ID and turn number.
+
+During the design process, the idea of having a track comparison feature was toyed with.Whilst the concept seemed good on paper, it felt too much for an initial app launch and required major alteration to the already stable code, which was already massively bloated in size.
+
+Perhaps this feature could be explored post launch to the public and serve as a future project to revise and improve on the skills learnt during the CS50 course.Alongside this a general review of code conciseness could be carried out to improve the readability of the code for potential future colaborators.
+
+Overall, I am happy with how this Flask app has turned out.The scope of the project has allowed me to confirm my understanding of the concepts taught during the CS50 course and provided me with a app I can continue to develop as part of my portfolio or even launch to a customer base with enough time invested.
--- app.py
+++ app.py
@@ -1,40 +1,267 @@
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, url_for, g
+from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
 
-from datetime import datetime
+
+# Create the db extension
+db = SQLAlchemy()
 
 # Configure application
 app = Flask(__name__, template_folder='templates')
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tracks.db"
+
+# Set secret key
+app.config['SECRET_KEY'] = 'f3cfe9ed8fae309f02079dbf'
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# initialize the app with the extension
+db.init_app(app)
+
+# Create the username table
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    hash = db.Column(db.String, nullable=False)
+# Create the tracks table
+class Tracks(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=False, nullable=False)
+    track_name = db.Column(db.String, unique=False, nullable=False)
+    track_img = db.Column(db.String, unique=False, nullable=False)
+    lap_time = db.Column(db.String, nullable=True)
+# Create the notes table
+class Notes(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=False, nullable=False)
+    track_name = db.Column(db.String, unique=False, nullable=False)
+    turn_no = db.Column(db.Integer, nullable=False)
+    event = db.Column(db.String, nullable=False)
+    note = db.Column(db.String, nullable=False)
+    ref_img = db.Column(db.String, nullable=True)
+# Creates schema
+with app.app_context():
+    db.create_all()
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session["user_id"] is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response    
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/tracks")
+def tracks():
+    user_in_session = session["user_id"]
+    tracks = Tracks.query.filter_by(user_id=user_in_session)
+    return render_template("index.html", tracks=tracks)
 
-@app.route("/register")
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
 
-@app.route("/login")
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("register.html", error="Must provide username")
+
+        # Ensure password was submitted
+        if not request.form.get("password"):
+            return render_template("register.html", error="Must provide password")
+
+        # Ensure password was confirmed
+        if request.form.get("password") != request.form.get("confirm_password"):
+            return render_template("register.html", error="Passwords do not match")
+
+        # Check for duplicate users then add the user's entry into the database if check passed
+        if User.query.filter_by(username=request.form.get("username")).first() is not None:
+            return render_template("register.html", error="Username already taken")
+
+        # Generate hash
+        user = request.form.get("username")
+        password = request.form.get("password")
+        hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+
+        # Add user to database
+        new_user = User(username=user, hash=hash)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect("/")
+    
+    else:
+        return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    session.clear()
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Ensure username was submitted
+        if not username:
+            return render_template("login.html", error="Username not entered. Please try again.")
+
+        # Ensure password was submitted
+        if not password:
+            return render_template("login.html", error="Password not entered. Please try again.")
+
+        # Query database for username
+        user = User.query.filter_by(username=username).first()
+
+        # Ensure username exists and password is correct
+        if not user or not check_password_hash(user.hash, password):
+            return render_template("login.html", error="Login details not correct. Please try again.")
+
+        # Remember which user has logged in
+        session["user_id"] = user.id
+
+        # Redirect user to home page
+        return redirect("/tracks")
+
+    # User reached route via GET (as by clicking a link or via redirect)
     return render_template("login.html")
 
-@app.route("/compare")
-def compare():
-    return render_template("compare.html")
-
-@app.route("/add")
+@app.route("/add", methods=["GET","POST"])
+@login_required
 def add():
-    return render_template("add.html")
+    if request.method == "POST":
+
+        # Get the user in session
+        user_in_session = session["user_id"]
+
+        # Ensure Track Name was submitted
+        if not request.form.get("track_name"):
+            return render_template("register.html", error="Must provide track name")
+
+        # Ensure Track Map was submitted
+        if not request.form.get("track_url"):
+            return render_template("register.html", error="Must provide track map link")
+
+        # Check for duplicate tracks with user then add the user's entry into the database if check passed
+        if Tracks.query.filter_by(track_name=request.form.get("track_name"),user_id=user_in_session).first() is not None:
+            return render_template("register.html", error="Track already added")
+
+        # Get form entries
+        track_name = request.form.get("track_name")
+        track_url = request.form.get("track_url")
+        lap_time = request.form.get("lap_time")
+
+        # Add track to database
+        new_track = Tracks(user_id=user_in_session, track_name=track_name, track_img=track_url, lap_time=lap_time)
+        db.session.add(new_track)
+        db.session.commit()
+        return redirect("/tracks")
+    
+    else:
+        return render_template("add.html")
 
 @app.route("/FAQ")
 def FAQ():
     return render_template("FAQ.html")
 
-@app.route("/notes")
-def notes():
-    return render_template("notes.html")
+@app.route("/notes/<track_name>", methods=["GET","POST"])
+@login_required
+def notes(track_name):
+    if request.method == "POST":
+
+        # Get the user in session
+        user_in_session = session["user_id"]
+
+        # Ensure Turn no. was submitted
+        if not request.form.get("turn_no"):
+            return render_template("notes.html", error="Must provide turn no.")
+        
+        # Ensure Event was submitted
+        if not request.form.get("event"):
+            return render_template("notes.html", error="Must provide turning event")
+        
+        # Ensure Note was submitted
+        if not request.form.get("note"):
+            return render_template("notes.html", error="Must provide note")
+
+        # Get form entries
+        turn_no = request.form.get("turn_no")
+        event = request.form.get("event")
+        note = request.form.get("note")
+        ref_img = request.form.get("img")
+
+        # Add note to database
+        new_track = Notes(user_id=user_in_session, track_name=track_name, turn_no=turn_no, event=event, note=note,ref_img=ref_img)
+        db.session.add(new_track)
+        db.session.commit()
+        return redirect("/tracks")
+    
+    else:
+        return render_template("notes.html")
+
+@app.route("/retime/<track_name>", methods=["GET","POST"])
+@login_required
+def retime(track_name):
+    if request.method == "POST":
+
+        # Get the user in session
+        user_in_session = session["user_id"]
+
+        track = Tracks.query.filter_by(track_name=track_name,user_id=user_in_session).first()
+        print(track)
+
+        # Ensure Lap Time was submitted
+        if not request.form.get("lap_time"):
+            return render_template("retime.html", error="Must provide lap time")
+
+        # Get form entries
+        lap_time = request.form.get("lap_time")
+        print(track_name)
+
+        # Update database entry
+        track.lap_time = lap_time
+        db.session.commit()
+        return redirect("/tracks")
+    
+    else:
+        return render_template("retime.html")
+
+@app.route("/view_notes/<track_name>")
+def view_notes(track_name):
+    user_in_session = session["user_id"]
+    notes = Notes.query.filter_by(user_id=user_in_session, track_name=track_name)
+    return render_template("view_notes.html", notes=notes)
+
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
 
 if __name__ == "__main__":
+    # Creates database schema
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
